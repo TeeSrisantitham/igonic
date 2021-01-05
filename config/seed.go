6264e8c15e0cd058c5ee455ffb5a76ebf7d7d67a
Tee <tee@yelllow.com>
2021-01-05 10:14:22 +0700

    Refactor seed

--- config/seed.go
+++ config/seed.go
@@ -15,19 +15,16 @@ type seed struct {
 	Articles []models.Article `yaml:"articles"`
 }
 
-// Seeds import seeds files into database by env yml file
-func Seeds(db *gorm.DB, env string) error {
-	raw, err := ioutil.ReadFile("db/seeds/" + env)
+func readENVFile(path string, seedInfo *seed) error {
+	raw, err := ioutil.ReadFile(path)
 	if err != nil {
 		return err
 	}
 
-	seedInfo := seed{}
-	err = yaml.Unmarshal(raw, &seedInfo)
-	if err != nil {
-		return err
-	}
+	return yaml.Unmarshal(raw, &seedInfo)
+}
 
+func insertToDB(db *gorm.DB, seedInfo seed) error {
 	v := reflect.ValueOf(seedInfo)
 
 	for i := 0; i < v.NumField(); i++ {
@@ -38,6 +35,17 @@ func Seeds(db *gorm.DB, env string) error {
 			}
 		}
 	}
-
 	return nil
+}
+
+// Seeds import seeds files into database by env yml file
+func Seeds(db *gorm.DB, env string) error {
+	var seedInfo seed
+	
+	err := readENVFile("db/seeds/" + env, &seedInfo)
+	if err != nil {
+		return err
+	}
+	
+	return insertToDB(db, seedInfo)
 }
